ae7f2f0ac367a66ced086e258f7082fe4b3f5161
Titanic880 <Warframeia1501@gmail.com>
2020-12-28 14:28:03 -0700

    Created a .sql file to store a basic user create and all functions related to the database outside of the code

---
+++ DataLayer.sql
@@ -0,0 +1,23 @@
+/* For security reasons the login is created disabled and with a random password. */
+USE [master]
+GO
+/****** Object:  Login [DataLayer_Admin]    Script Date: 2020-12-28 2:25:51 PM ******/
+CREATE LOGIN [DataLayer_Admin] WITH PASSWORD=N'cWn4y1oisN40FqazsQmqoyvkcZ9ftRCq/BWxvixYmoc=', DEFAULT_DATABASE=[DataLayer], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+GO
+
+ALTER LOGIN [DataLayer_Admin] DISABLE
+GO
+
+create Database DataLayer;
+go
+use DataLayer;
+go
+/*
+REMOVAL FUNCTIONS, DELETES ALL EXISTING DB DATA
+use master;
+go
+
+
+Drop Database DataLayer;
+go
+*/
