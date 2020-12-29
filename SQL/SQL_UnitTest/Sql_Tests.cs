cefb6a1456f5cd17cf8f559a16136af3821aedf6
Titanic880 <Warframeia1501@gmail.com>
2020-12-29 02:03:26 -0700

    Completed unit tests for current version of the Sql Class

--- SQL/SQL_UnitTest/Sql_Tests.cs
+++ SQL/SQL_UnitTest/Sql_Tests.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using DataLayer.SQL;
 using System.Configuration;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Data.SqlClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 /// <summary>
 /// If the Connected Tests Fail, Everything else has a high Chance of failing
@@ -15,10 +15,11 @@ namespace SQL_UnitTest
     [TestClass]
     public class Sql_Tests
     {
+        #region Connection/Tbl check
         [TestMethod]
         public void Connected_STRING()
         {
-           Assert.IsTrue(Sql_Functions.SetConn(ConfigurationManager.AppSettings.Get("Connection")));
+            Assert.IsTrue(Sql_Functions.SetConn(ConfigurationManager.AppSettings.Get("Connection")));
         }
 
         [TestMethod]
@@ -41,6 +42,23 @@ namespace SQL_UnitTest
             Assert.IsTrue(Sql_Functions.Connected);
         }
 
+        #endregion Connection/Tbl check
+
+        #region Execution Methods
+        [TestMethod]
+        public void NonQuery_String()
+        {
+            Sql_Functions.SetConn(ConfigurationManager.AppSettings.Get("Connection"));
+
+            string TableName = "NonQueryTest";
+
+            string test = $"Create Table {TableName}([id] int);";
+            if (Sql_Functions.Check_Table(TableName))
+                test = $"Drop Table {TableName}";
+
+            Assert.AreNotEqual("--", Sql_Functions.RunNonQuery(test).ToString().Substring(0, 2));
+            Assert.IsTrue(Sql_Functions.Connected);
+        }
         [TestMethod]
         public void NonQuery_SqlCommand()
         {
@@ -50,13 +68,58 @@ namespace SQL_UnitTest
 
             string test = $"Create Table {TableName}([id] int);";
             if (Sql_Functions.Check_Table(TableName))
-                test = $"Drop Table {TableName}";    
+                test = $"Drop Table {TableName}";
 
-            SqlCommand comm = new SqlCommand(test);
-            object result = Sql_Functions.RunNonQuery(comm);
-            string res = result.ToString();
-            Assert.AreNotEqual("--", res.Substring(0,2));
+            SqlCommand cmd = new SqlCommand(test);
+            Assert.AreNotEqual("--", Sql_Functions.RunNonQuery(cmd).ToString().Substring(0, 2));
             Assert.IsTrue(Sql_Functions.Connected);
         }
+
+        [TestMethod]
+        public void Scalar_String()
+        {
+            Sql_Functions.SetConn(ConfigurationManager.AppSettings.Get("Connection"));
+
+            string TableName = "ScalarTest";
+
+            string test = $"Create Table {TableName}([id] int);";
+            if (Sql_Functions.Check_Table(TableName))
+                test = $"Drop Table {TableName}";
+
+            Assert.AreNotEqual("--",Sql_Functions.RunScalar(test));
+            Assert.IsTrue(Sql_Functions.Connected);
+        }
+        [TestMethod]
+        public void Scalar_SqlCommand()
+        {
+            Sql_Functions.SetConn(ConfigurationManager.AppSettings.Get("Connection"));
+            string TableName = "ScalarTest";
+
+            string test = $"Create Table {TableName}([id] int);";
+            if (Sql_Functions.Check_Table(TableName))
+                test = $"Drop Table {TableName}";
+
+            SqlCommand cmd = new SqlCommand(test);
+            Assert.AreNotEqual("--", Sql_Functions.RunScalar(cmd));
+            Assert.IsTrue(Sql_Functions.Connected);
+        }
+
+        [TestMethod]
+        public void Query()
+        {
+            Sql_Functions.SetConn(ConfigurationManager.AppSettings.Get("Connection"));
+
+            string TableName = "QueryTest";
+            string[] Create_Query = {$"Create Table {TableName}(id int);" ,$"INSERT INTO {TableName} Values(1);"};
+            foreach (string a in Create_Query)
+                Sql_Functions.RunNonQuery(a);
+
+            string Query = $"Select * from {TableName}";
+
+            Assert.IsNotNull(Sql_Functions.Query(Query));
+            Assert.IsTrue(Sql_Functions.Connected);
+        }
+
+        #endregion Execution Methods
     }
 }
